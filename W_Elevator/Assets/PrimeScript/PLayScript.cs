c1e608a89bd075d32ef5856dd10c3ef7a7d004ab
SCR 3 <scr3@student.nci.col>
2018-04-04 12:39:08 +0100

    added menu screens

---
+++ W_Elevator/Assets/PrimeScript/PLayScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PLayScript : MonoBehaviour {
+	public Transform buttonPanel;
+	public Button playButton, quitButton;
+	public Slider loadSlider;
+	private float progress;
+
+	// Use this for initialization
+	void Start () {
+
+		loadSlider.gameObject.SetActive (false);
+		buttonPanel.gameObject.SetActive (true);
+		playButton.onClick.AddListener (Play);
+		quitButton.onClick.AddListener (QuitGame);
+	}
+
+
+	public void Play(){
+		StartCoroutine (LoadScene (1));
+	}
+
+	IEnumerator LoadScene(int index){
+		loadSlider.gameObject.SetActive (true);
+		buttonPanel.gameObject.SetActive (false);
+		AsyncOperation sceneLoad =  SceneManager.LoadSceneAsync (index);
+		while (!sceneLoad.isDone) {
+			progress = Mathf.Clamp01(sceneLoad.progress);
+			loadSlider.value = progress;
+
+			yield return null;
+		}
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+}
---
+++ W_Elevator/Assets/PrimeScript/PauseScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour {
+	public GameObject pauseMenu;
+	public Button contBtn, quitBtn;
+	bool isPaused = false;
+	// Use this for initialization
+	void Start () {
+		quitBtn.onClick.AddListener (QuitGame);
+		contBtn.onClick.AddListener (Continue);
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.P)) {
+			isPaused = !isPaused;
+		}
+		if (isPaused) {
+			Time.timeScale = 0;
+			pauseMenu.SetActive (true);
+		} else {
+			Time.timeScale = 1;
+			pauseMenu.SetActive (false);
+		}
+	}
+
+	void Continue(){
+		pauseMenu.SetActive (false);
+	}
+
+	void QuitGame(){
+		StartCoroutine (LoadScene (0));
+	}
+	IEnumerator LoadScene(int index){
+		AsyncOperation a = SceneManager.LoadSceneAsync (index);
+		while (!a.isDone) {
+			yield return null;
+		}
+	}
+}
