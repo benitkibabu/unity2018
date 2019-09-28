e87a346956beece2fccbbc4e4366b21cc491e50f
benit kibabu <benitkibabu@gmail.com>
2019-09-28 18:03:29 +0100

    the first commit

---
+++ W_Elevator/Assets/FlashLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour {
+	public Light flahsLight;
+	// Use this for initialization
+	void Start () {
+		flahsLight.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (flahsLight != null) {
+			if (Input.GetKeyDown (KeyCode.Mouse0)) {
+				flahsLight.enabled = !flahsLight.enabled;
+			}
+		}
+	}
+}
